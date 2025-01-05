26e8992ce2948301d33b38c1dd8a8e71e437e241
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2025-01-05 20:32:06 +0300

    Updaed files

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,6 +8,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <App />
-    
+
   </StrictMode>,
 )
